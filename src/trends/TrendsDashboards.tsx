501779107c8d00a216fda1009e6f31cd376a15c1
SmallScale <29871559+SmallScale@users.noreply.github.com>
2021-05-15 19:22:42 +0100

    Feat: Integrating more features and deployed API

--- src/trends/TrendsDashboards.tsx
+++ src/trends/TrendsDashboards.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Grid } from "@material-ui/core";
+import React, { useState } from "react";
+import { Button, Grid } from "@material-ui/core";
 import Trend from "./Trend";
 import { IndicatorCollection } from "ape-trends/dist/utils";
 
@@ -8,31 +8,67 @@ type TrendsDashboardProps = {
 };
 
 const TrendsDashboard: React.FC<TrendsDashboardProps> = ({ apeTrends }) => {
+  const fullList = Object.values(apeTrends).sort(
+    (trendA, trendB) => trendB.mentions - trendA.mentions
+  );
+  const mostPopular = Object.values(apeTrends)
+    .sort((trendA, trendB) => trendB.mentions - trendA.mentions)
+    .splice(0, 10);
+  console.log(mostPopular);
+  const [showFullList, setShowFullList] = useState<boolean>(false);
+
+  const changeShowFullList = (): void => setShowFullList(!showFullList);
+
   return (
-    <Grid container spacing={2} direction="row" justify="center">
-      {Object.values(apeTrends).map(({ name, symbol, buy, hold, sell }, i) => {
-        const possibleTrends = [
-          { name: "buy", value: buy },
-          { name: "hold", value: hold },
-          { name: "sell", value: sell },
-        ];
-        return (
-          <Grid item style={{ width: "25%", height: "25%" }} xs={6}>
-            <Trend
-              name={name}
-              symbol={symbol}
-              trend={
-                possibleTrends.sort((a, b) => b.value - a.value)[0].name as
-                  | "buy"
-                  | "hold"
-                  | "sell"
-              }
-              i={i + 1}
-            />
-          </Grid>
-        );
-      })}
-    </Grid>
+    <>
+      <Grid container spacing={2} direction="row" justify="center">
+        <Grid item>
+          <Button
+            variant="contained"
+            color={showFullList ? "default" : "primary"}
+            onClick={changeShowFullList}
+          >
+            List most popular
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button
+            variant="contained"
+            color={showFullList ? "primary" : "default"}
+            onClick={changeShowFullList}
+          >
+            List all
+          </Button>
+        </Grid>
+      </Grid>
+      <div style={{ height: "2rem" }} />
+      <Grid container spacing={2} direction="row" justify="flex-start">
+        {Object.values(showFullList ? fullList : mostPopular).map(
+          ({ name, symbol, buy, hold, sell }, i) => {
+            const possibleTrends = [
+              { name: "buy", value: buy },
+              { name: "hold", value: hold },
+              { name: "sell", value: sell },
+            ];
+            return (
+              <Grid item style={{ width: "25%", height: "25%" }} xs={6}>
+                <Trend
+                  name={name}
+                  symbol={symbol}
+                  trend={
+                    possibleTrends.sort((a, b) => b.value - a.value)[0].name as
+                      | "buy"
+                      | "hold"
+                      | "sell"
+                  }
+                  i={i + 1}
+                />
+              </Grid>
+            );
+          }
+        )}
+      </Grid>
+    </>
   );
 };
 
